1f79da8c8c243b73354997b9074ab67220606652
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-01-25 18:08:48 +0200

    made third app, model, migration and made an article on a web-site

---
+++ news/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Articles(models.Model):
+    title = models.CharField("Title", max_length=50)
+    announcment = models.CharField("Announcment",max_length=250)
+    full_text = models.TextField("An acrticle")
+    date = models.DateTimeField("Date of publication")
+
+    def __str__(self):
+        return self.title
+# Create your models here.
